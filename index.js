58803b9bc33068a7bd81b1c548268550a93e0b0c
Pralhad Shrestha <pralhad.shrestha05@gmail.com>
2016-11-10 17:52:37 +0545

    Elixir implementation of typescript

---
+++ index.js
@@ -0,0 +1,45 @@
+var Elixir = require('laravel-elixir');
+var tsconfig = require('./tsconfig.json');
+var Typescript = require('typescript');
+var config = Elixir.config;
+
+/*
+ |----------------------------------------------------------------
+ | Typescript Compilation Task
+ |----------------------------------------------------------------
+ |
+ | This task will compile your Typescript.
+ |
+ */
+
+Elixir.config.js.typescript = {
+	folder: 'typescript',
+	search: '/**/*.ts',
+	plugin: Typescript,
+	pluginOptions: {}
+};
+
+
+Elixir.extend('typescript', function (src,output,options) {
+	var paths = getPaths(src, output);
+	new Elixir.Task('typescript', function () {
+		return gulp
+			.src(paths.src.baseDir)
+			.pipe(Typescript(tsconfig.compilerOptions))
+			.pipe(gulp.dest(paths.output.baseDir));
+	});
+});
+
+/**
+ * Prep the Gulp src and output paths.
+ * Source: https://github.com/JeffreyWay/laravel-elixir-stylus/blob/master/index.js
+ *
+ * @param  {string|array} src
+ * @param  {string|null}  output
+ * @return {object}
+ */
+var getPaths = function(src, output) {
+	return new Elixir.GulpPaths()
+		.src(src, Elixir.config.get('assets.js.typescript.folder'))
+		.output(output || Elixir.config.get('public.js.outputFolder'), 'app.js');
+};
